3a10d988c4b1468f65605bd6aa88c28d2b66b0b3
Lidor Blustein <liidorr@gmail.com>
2023-07-15 23:53:26 +0300

    added an option to choose items picked, and to clear all

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-6bea0-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "hhttps://playground-6bea0-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+const pickedListInDB = ref(database, "pickedList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 const pickedListEl = document.getElementById("picked-list")
-let isPicked = false
+const clearAllEl = document.getElementById("clear-all")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
@@ -41,8 +42,30 @@ onValue(shoppingListInDB, function(snapshot) {
     }
 })
 
+onValue(pickedListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearPickedListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToPickedListEl(currentItem)
+        }    
+    } else {
+        pickedListEl.innerHTML = ""
+    }
+})
+
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
+}
+
+function clearPickedListEl() {
+    pickedListEl.innerHTML = ""
 }
 
 function clearInputFieldEl() {
@@ -58,24 +81,44 @@ function appendItemToShoppingListEl(item) {
     newEl.textContent = itemValue
     
     newEl.addEventListener("click", function() {
-        // remove the item
-        //let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        //remove(exactLocationOfItemInDB)
-
-        // make the item go down
-        if(!isPicked){
-            pickedListEl.append(newEl)
-            newEl.style.backgroundColor = "#97A7B3"
-            newEl.style.color = "black"
-            isPicked = true
-        }
-        else{
-            shoppingListEl.append(newEl)
-            newEl.style.backgroundColor = "#FFFDF8"
-            newEl.style.color = "#432000"
-            isPicked = false
-        }
+        // add the item to pickedList
+        pickedListEl.append(newEl)
+        let inputValue = newEl.textContent
+        push(pickedListInDB, inputValue)
+        // remove the item from shoppingList
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItemInDB)
     })
     
     shoppingListEl.append(newEl)
 }
+
+function appendItemToPickedListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    // change the item's color to look picked
+    newEl.style.backgroundColor = "#97A7B3"
+    newEl.style.color = "black"
+    
+    newEl.addEventListener("click", function() {
+        // add the item to shoppingList
+        shoppingListEl.append(newEl)
+        let inputValue = newEl.textContent
+        push(shoppingListInDB, inputValue)
+        // remove the item from pickedList
+        let exactLocationOfItemInDB = ref(database, `pickedList/${itemID}`)
+        remove(exactLocationOfItemInDB)
+    })
+    
+    pickedListEl.append(newEl)
+}
+
+clearAllEl.addEventListener("click", function() {
+    // remove all items
+    remove(shoppingListInDB)
+    remove(pickedListInDB)
+})
